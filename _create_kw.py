d9c5d3d2cb4f6c5f33f1c9166298200614a63555
ye-shun-guang <ye.shun.guang@example.com>
2026-07-16 10:06:43 +0800

    chore: remove temp files and SQLite WAL from tracking

--- _create_kw.py
+++
@@ -1,8 +0,0 @@
-﻿import os
-path = r'D:\小光工作区\projects\stock-classroom\frontend\kline_widget.py'
-
-# Only write if file doesn't exist (to avoid overwriting)
-if not os.path.exists(path):
-    print(f'File does not exist, will create: {path}')
-else:
-    print(f'File exists, size: {os.path.getsize(path)}')
